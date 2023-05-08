90aace1967168e8753f48f401e86291476e692e8
Gonzalo Stoll <stollgonzalo@gmail.com>
2023-05-08 23:59:39 +0200

    Recommended config

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ module.exports = {
   rules: {
     'no-namespace-import': require('./rules/no-namespace-import'),
   },
+  configs: {
+    recommended: {
+      plugins: ['react-namespace-import'],
+      rules: {
+        'react-namespace-import/no-namespace-import': 'error',
+      },
+    },
+  },
 }
